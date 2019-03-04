1206a2b800ebf37f25f893ecac1b0eb3af3422cd
prytherchs <sidneyprytherch@gmail.com>
2019-03-04 13:09:25 -0500

    Add Crossword Activity and button to switch activities

--- app/src/main/java/edu/wit/mobileapp/languagetravelapp/HomeActivity.java
+++ app/src/main/java/edu/wit/mobileapp/languagetravelapp/HomeActivity.java
@@ -1,7 +1,11 @@
 package edu.wit.mobileapp.languagetravelapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -9,5 +13,12 @@ public class HomeActivity extends AppCompatActivity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_home);
+
+    Button crosswordButton = findViewById(R.id.crossword_button);
+    crosswordButton.setOnClickListener((View v) -> {
+      Log.v("MyApp", "Crossword button is clicked");
+      Intent intent = new Intent(HomeActivity.this, CrosswordActivity.class);
+      startActivity(intent);
+    });
   }
 }
