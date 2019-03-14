34705cbf0e7e29b5e30a3c473f5b0d83498c80b9
prytherchs <sidneyprytherch@gmail.com>
2019-03-13 21:06:13 -0400

    rename files in the layout file
    alphabetically, things are arranged a little better now. Things that are simple activities are formatted as "activity_etc" and things related to crossword are "crossword_etc" and travel is "travel_etc"

--- app/src/main/java/edu/wit/mobileapp/languagetravelapp/FoodFragment.java
+++ app/src/main/java/edu/wit/mobileapp/languagetravelapp/FoodFragment.java
@@ -13,7 +13,7 @@ public class FoodFragment extends Fragment {
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_food, container, false);
+        View view = inflater.inflate(R.layout.travel_fragment_food, container, false);
         // Inflate the layout for this fragment
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(view.getContext(),
                 android.R.layout.simple_list_item_1);
