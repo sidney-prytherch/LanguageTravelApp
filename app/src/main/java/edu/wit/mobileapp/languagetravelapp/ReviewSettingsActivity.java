ce1c81fa930c5ab9a677b504eccf0c0619689e64
prytherchs <sidneyprytherch@gmail.com>
2019-03-19 21:56:08 -0400

    Add and update activities

---
+++ app/src/main/java/edu/wit/mobileapp/languagetravelapp/ReviewSettingsActivity.java
@@ -0,0 +1,13 @@
+package edu.wit.mobileapp.languagetravelapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ReviewSettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_review_settings);
+    }
+}
