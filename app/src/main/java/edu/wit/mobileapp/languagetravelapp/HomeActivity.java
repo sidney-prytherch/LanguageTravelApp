b8b70940db7c6389565e11cb8b72c10fed3e423c
Sidney Prytherch <sidneyprytherch@gmail.com>
2019-03-27 17:43:08 -0400

    add animated logo

--- app/src/main/java/edu/wit/mobileapp/languagetravelapp/HomeActivity.java
+++ app/src/main/java/edu/wit/mobileapp/languagetravelapp/HomeActivity.java
@@ -1,7 +1,14 @@
 package edu.wit.mobileapp.languagetravelapp;
 
 import android.content.Intent;
+import android.graphics.drawable.Animatable;
+import android.graphics.drawable.AnimatedVectorDrawable;
+import android.graphics.drawable.Drawable;
+import android.os.Handler;
+import android.os.Looper;
 import android.support.design.widget.NavigationView;
+import android.support.graphics.drawable.Animatable2Compat;
+import android.support.graphics.drawable.AnimatedVectorDrawableCompat;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -9,8 +16,25 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ImageView;
 
 public class HomeActivity extends AppCompatActivity {
+
+//    final AnimatedVectorDrawableCompat av = AnimatedVectorDrawableCompat.create(getApplicationContext(), R.drawable.animated_logo);
+//
+//    ImageView icon = findViewById(R.id.travel_icon);
+//        icon.setImageDrawable(av);
+//        av.registerAnimationCallback(new Animatable2Compat.AnimationCallback() {
+//        private final Handler fHandler = new Handler(Looper.getMainLooper());
+//
+//        @Override
+//        public void onAnimationEnd(Drawable drawable) {
+//            AnimatedVectorDrawable avd = (AnimatedVectorDrawable) drawable;
+//            fHandler.post(avd::start);
+//        }
+//    });
+//    final Animatable animatable = (Animatable) icon.getDrawable();
+//        animatable.start();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +53,7 @@ public class HomeActivity extends AppCompatActivity {
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(new NavItemSelectedListener(drawer, getApplicationContext(), this));
         navigationView.getMenu().getItem(0).setChecked(true);
+
 
     }
 
